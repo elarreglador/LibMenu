a35f2021b9a5978bb5bcc6d4da630ad574ff968e
elarreglador <elarreglador@protonmail.com>
2024-11-01 11:47:57 +0100

    Revisados ejemplos de uso

--- Menu.java
+++ Menu.java
@@ -8,70 +8,63 @@ public class Menu {
 		Scanner teclado = new Scanner(System.in);
 		// Array con enunciado y opciones
 		String[] opciones = {
-			"Cabecera del enunciado:",
-			"Opcion A",
-			"Opcion B",
-			"Opcion C"
+				"Cabecera del enunciado:",
+				"Opcion A",
+				"Opcion B",
+				"Opcion C"
 		};
 		// menu() nos devuelve un int con la opcion seleccionada
-		System.out.println("Has seleccionado la opcion: " + menu(opciones, teclado));
-		
+		System.out.println("Has seleccionado la opcion: " + Menu.menu(opciones, teclado));
+
 		// guardamos la seleccion en una variable
 		@SuppressWarnings("unused")
-		int seleccion = menu(opciones, teclado);
-		
+		int seleccion = Menu.menu(opciones, teclado);
+
 		// usamos switch para gestionar la respuesta del menu
-		switch (menu(opciones, teclado)) {
-	        case 0:
-	        	System.out.println("opcion cero");
-	        	break;
-	        case 1:
-	        	System.out.println("opcion uno");
-	        	break;
-	        case 2:
-	        	System.out.println("opcion dos");
-	        	break;
-	        case 3:
-	        	System.out.println("opcion tres");
-	        	break;
+		switch (Menu.menu(opciones, teclado)) {
+			// no hay un case 0
+			case 1:
+				System.out.println("opcion uno");
+				break;
+			case 2:
+				System.out.println("opcion dos");
+				break;
+			case 3:
+				System.out.println("opcion tres");
+				break;
 		}
-		
+
 	}
-	
-	
-	
-	
+
 	// MUESTRA UN MENU CON LOS STRINGS DEL PARAMETRO
 	public static int menu(String[] opciones, Scanner teclado) {
 		// Usamos un único Scanner en toda la aplicacion
 		// opciones[0] es la cabecera
-		
-        int retorno = -1;
 
-        while (true) {
-        	
-            System.out.println(opciones[0] + "\n");
-            for (int i = 1; i < opciones.length; i++) {
-                System.out.printf("   %d) %s%n", i, opciones[i]);
-            }
+		int retorno = -1;
 
-            try {
-                System.out.print("\n: ");
-                retorno = Integer.parseInt(teclado.nextLine());
+		while (true) {
 
-                if (retorno >= 1 && retorno < opciones.length) {
-                    break; // Opción válida
-                } else {
-                    System.out.println("Opción no válida. Inténtalo de nuevo.");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Entrada no válida. Inténtalo de nuevo.");
-            }
-            
-        }
-        return retorno;
+			System.out.println(opciones[0] + "\n");
+			for (int i = 1; i < opciones.length; i++) {
+				System.out.printf("   %d) %s%n", i, opciones[i]);
+			}
+
+			try {
+				System.out.print("\n: ");
+				retorno = Integer.parseInt(teclado.nextLine());
+
+				if (retorno >= 1 && retorno < opciones.length) {
+					break; // Opción válida
+				} else {
+					System.out.println("Opción no válida. Inténtalo de nuevo.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Entrada no válida. Inténtalo de nuevo.");
+			}
+
+		}
+		return retorno;
 	}
-	
-	
 
 }
